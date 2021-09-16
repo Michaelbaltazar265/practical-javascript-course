b59714659446adf224b88aec3e2b332174f8cdf7
Michael <michaelbaltazar265@gmail.com>
2021-09-16 18:01:35 +0000

    basics of the course

---
+++ basics/app.js
@@ -0,0 +1,16 @@
+
+function printName (first, last) { 
+    console.log(first + " " + last);
+    alert(first + " " + last)
+}
+
+printName("John", "Smith")
+
+//////////////////////////// 
+// Operators + - * / % ++ -- 
+
+let x = 25;
+// 25 + 5
+// is the same as writing this 
+x += 5; 
+console.log(x);
